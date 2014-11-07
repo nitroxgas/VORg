97172550516fe72198dfc7c40877fa512a5ca49c
nitroxgas <nitroxgas@gmail.com>
2014-11-07 19:16:14 -0200

    Just backup last modifications CardListItem

--- mobile/src/main/java/br/com/casadalagoa/vof/about_activity.java
+++ mobile/src/main/java/br/com/casadalagoa/vof/about_activity.java
@@ -2,6 +2,8 @@ package br.com.casadalagoa.vof;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 
 public class about_activity extends Activity {
@@ -10,6 +12,16 @@ public class about_activity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_activity);
+        ImageButton mImageButton;
+        mImageButton = (ImageButton) findViewById(R.id.btn_google_play);
+        mImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 
 
